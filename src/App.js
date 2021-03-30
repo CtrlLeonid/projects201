6bcc83ac017f31fa97f5eb5864d91b6d4039d978
CtrlLeonid <umkasu@gmail.com>
2021-03-30 12:28:24 +0300

    first commit1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
 import {Route, BrowserRouter, NavLink} from "react-router-dom";
 
-function ConyactUs(){
+function Menu(){
+    return(
+        <nav className="nav">
+            <NavLink className="nav-link active" aria-current="page" to="/">Главная</NavLink>
+            <NavLink className="nav-link" to="/about">О нас</NavLink>
+            <NavLink className="nav-link" to="/contact-us">Контакты</NavLink>
+        </nav>
+    )
+}
+
+function Info() {
+    
+}
+
+function ContactUs(){
     return (
-    <div className="container my-5">
-        <form action="">
-            <div className="mb-3"><input type="text" className="form-control"/></div>
-            <div className="mb-3"><input type="text" className="form-control"/></div>
-            <div className="mb-3"><input type="text" className="form-control"/></div>
-            <div className="mb-3"><input type="text" className="form-control"/></div>
-        </form>
-    </div>
+        <div className="container my-5">
+            <form action="">
+                <div className="mb-3"><input type="text"  placeholder="Введите Emai" className="form-control"/></div>
+                <div className="mb-3"><input type="text" placeholder="Как к вам обращаться" className="form-control"/></div>
+                <div className="mb-3"><textarea type="text" placeholder="Введите текст" className="form-control"/></div>
+                <div className="mb-3"><input type="submit" className="form-control btn btn-primary"/></div>
+            </form>
+        </div>
     )
 }
 
@@ -18,15 +32,11 @@ function App() {
   return (
     <div className="App">
         <BrowserRouter>
-            <nav className="nav">
-                <NavLink className="nav-link active" aria-current="page" to="#">Главная</NavLink>
-                <NavLink className="nav-link" to="/about">О нас</NavLink>
-                <NavLink className="nav-link" to="contact-us">Контакты</NavLink>
-            </nav>
+            <Menu/>
 
             <Route exact path="/" render={()=><h1>Главная стрница</h1>}/>
             <Route path="/about" render={()=><h1>О нас</h1>}/>
-            <Route path="/contact-us"render={()=><h1>Страница контакты</h1>}/>
+            <Route path="/contact-us"render={()=><h1><ContactUs/></h1>}/>
         </BrowserRouter>
     </div>
   );
